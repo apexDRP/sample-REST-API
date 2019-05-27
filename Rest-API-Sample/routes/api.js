10a8bfdec8a3b31b4ebf30ed787b3c23c0e2caf3
apexDRP <48053145+apexDRP@users.noreply.github.com>
2019-05-28 03:28:44 +0530

    Update api.js

--- Rest-API-Sample/routes/api.js
+++ Rest-API-Sample/routes/api.js
@@ -3,16 +3,19 @@ const router = express.Router();
 const Song = require('../song');
 const songs = [];
 
+//add a new song to songs array
 router.post('/songs', (req, res) => {
     const song = new Song(Date.now(), req.body.sTitle, req.body.sArtist, req.body.sGenre);
     songs.push(song);
     res.status(200).send(song);
 });
 
+//retrieve all songs from songs array
 router.get('/songs', (req, res) => {
     res.status(200).send(songs);
 });
 
+//update the song that matches given id
 router.put('/songs/:id', (req, res) => {
     const index = songs.findIndex(song => song.id === parseInt(req.params.id));
     if(index > -1) {
@@ -23,6 +26,7 @@ router.put('/songs/:id', (req, res) => {
     res.status(200).send(songs[index]);
 });
 
+//delete the song that matches given id
 router.delete('/songs/:id', (req, res) => {
     const index = songs.findIndex(song => song.id === parseInt(req.params.id));
     let song ='';
@@ -32,5 +36,5 @@ router.delete('/songs/:id', (req, res) => {
     }
     res.status(200).send(song);
 });
-
+//export the router to use in index.js
 module.exports = router;
